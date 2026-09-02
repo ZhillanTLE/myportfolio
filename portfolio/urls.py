fea3895d1b627393af4c0338c1b57ccc60dfcfaa
Zhillan <zhillanbaniaksa@gmail.com>
2026-09-02 17:59:00 +0700

    feat: Tutorial 01

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myportofolio project.
+URL configuration for portfolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
@@ -11,12 +11,15 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 
+from portfolio import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
